bc0d2406a027587a70b35544c54e366a755ecbea
Fionn McCloskey <fionn.mccloskey@klasgroup.com>
2026-03-05 12:25:18 +0000

    Add control for font size

--- .wezterm.lua
+++ .wezterm.lua
@@ -57,10 +57,23 @@ return {
     -- },
     -- Activate the scheme
     -- color_scheme = 'Compline'
-    color_scheme = 'nord'
+    color_scheme = 'nord',
 
+    mouse_bindings = {
+        -- Scrolling up while holding CTRL increases the font size
+        {
+            event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+            mods = 'CTRL',
+            action = act.IncreaseFontSize,
+        },
 
-
+        -- Scrolling down while holding CTRL decreases the font size
+        {
+            event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+            mods = 'CTRL',
+            action = act.DecreaseFontSize,
+        },
+    },
 }
 
 
