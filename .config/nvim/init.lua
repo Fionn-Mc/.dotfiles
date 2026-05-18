66df0f45c393eca326e1e6a517c3f3591e54698e
Fionn McCloskey <fmccloskey@anduril.com>
2026-05-18 12:56:40 +0100

    Set newer nvim to inherit colours from wezterm

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,13 +12,14 @@ vim.g.netrw_banner = 0
 vim.g.netrw_browse_split = 4
 vim.g.netrw_altv = 1
 vim.g.netrw_liststyle = 3
-vim.g.netrw_list_hide = vim.fn["netrw_gitignore#Hide"]() .. [[,\(^\|\s\s\)\zs\.\S\+]]
+-- vim.g.netrw_list_hide = vim.fn["netrw_gitignore#Hide"]() .. [[,\(^\|\s\s\)\zs\.\S\+]]
 
 -- cosmetic
 vim.o.cursorcolumn = true
 vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.termguicolors = false
 
 -- code preferences
 vim.o.shiftwidth = 4
