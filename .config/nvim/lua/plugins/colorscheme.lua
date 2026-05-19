9efb38d1ebdb06c97d3e3f0dd45ad048cccf8e9c
Fionn McCloskey <fmccloskey@anduril.com>
2026-05-19 10:10:13 +0100

    Inherit nvim colour scheme from wezterm

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- disable tokyonight so we use terminal colors
+  { "folke/tokyonight.nvim", enabled = false },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "default",
+    },
+  },
+}
