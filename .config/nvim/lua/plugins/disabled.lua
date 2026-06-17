6a567113a08f456e3e723ca716931ca5620123dc
Fionn McCloskey <fmccloskey@anduril.com>
2026-06-17 12:08:18 +0100

    Disable some lazyvim plugins

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/tokyonight.nvim", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "saghen/blink.cmp", enabled = false },
+}
