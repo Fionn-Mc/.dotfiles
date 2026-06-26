98310f5b2c417ffa4249fef996ef2dc2faa630fe
Fionn McCloskey <fmccloskey@anduril.com>
2026-06-26 11:57:44 +0100

    Change relative size of snacks file picker

---
+++ .config/nvim/lua/plugins/picker.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        -- enlarge the picker overlay to fill most of the terminal
+        layout = {
+          layout = {
+            width = 0.95,
+            height = 0.95,
+          },
+        },
+      },
+    },
+  },
+}
