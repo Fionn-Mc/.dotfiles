ec954f219957428a43dfd4cc8e7b7d7c4f867de5
Fionn McCloskey <fmccloskey@anduril.com>
2026-09-04 14:33:08 +0100

    reinstate nice command bar

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
   -- { "folke/tokyonight.nvim", enabled = false },
   { "nvim-mini/mini.pairs", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "saghen/blink.cmp", enabled = false },
 }
